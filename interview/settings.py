35e0b49044ca0b72ceafb05174023fe21de21c6b
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-07-20 23:31:16 +0330

    add whitelisted origins

--- interview/settings.py
+++ interview/settings.py
@@ -154,9 +154,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    # '*',
+]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 CORS_PREFLIGHT_MAX_AGE = 10
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
