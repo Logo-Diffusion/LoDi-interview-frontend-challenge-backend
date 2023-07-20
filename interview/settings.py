df6d0ea8140e7890e13fc815da2a0d18c27cbd9d
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-07-20 22:51:36 +0330

    Change order

--- interview/settings.py
+++ interview/settings.py
@@ -49,10 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
     'django_filters',
-    'corsheaders',
     'questionnaire',
 ]
 
