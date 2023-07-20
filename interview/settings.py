1d0f64d757c3a1fbf4e8a68bf24778ebf9d2540f
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-07-20 23:15:09 +0330

    Add localhost to allowed origins

--- interview/settings.py
+++ interview/settings.py
@@ -152,8 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_URLS_REGEX = r"^/api/.*$"
 
 CORS_ALLOWED_ORIGINS = [
-    # 'http://localhost:3000',
-    # 'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
     # '*',
 ]
 
