94d9c865420b10a2c179463574a8e3de11fa7981
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-07-20 23:12:04 +0330

    Add allowed origins

--- interview/settings.py
+++ interview/settings.py
@@ -149,9 +149,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_URLS_REGEX = r"^/api/.*$"
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r".*",
+# CORS_URLS_REGEX = r"^/api/.*$"
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    '*',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
